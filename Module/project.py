2123cbe7e3b06dcf7328b731aa2c2e76d6269f85
mapeee <marcus_peter@gmx.de>
2020-04-21 10:59:30 +0200

    option to keep all columns when finishing

--- Module/project.py
+++ Module/project.py
@@ -22,13 +22,26 @@ def project_xy(in_x, in_y, transformer):
     return x_out, y_out
 
 
-def project_table(indata, in_id, in_x, in_y, in_proj, out_proj):
+def project_table(indata, in_id, in_x, in_y, in_proj, out_proj, **optional):
     transformer = Transformer.from_crs(in_proj, out_proj, always_xy=True)
+    try: all_f = optional["all_f"]
+    except KeyError: all_f = False
     proj_list = []
+
     for index, row in indata.iterrows():
         x_out, y_out = project_xy(row[in_x],row[in_y],transformer)
-        id_out = row[in_id]
-        proj_list.append([id_out,x_out, y_out])
-        
-    proj_list = pd.DataFrame(proj_list, columns =[in_id, in_x, in_y])        
+        values = []
+
+        if all_f == True:
+            for i in indata.columns:
+                values.append(row[i])
+            values.extend([x_out, y_out])               
+            proj_list.append(values)
+            col = list(indata.columns)
+            col.extend([in_x+"_proj", in_y+"_proj"])
+        else:
+            values.extend([row[in_id],row[in_x],row[in_y],x_out, y_out])            
+            proj_list.append(values)
+            col = [in_id, in_x, in_y, in_x+"_proj", in_y+"_proj"]
+    proj_list = pd.DataFrame(proj_list, columns =col)           
     return proj_list
