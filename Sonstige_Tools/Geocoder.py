9ca394196d7f5c55e7ea111e372d60b15e32933b
mapeee <marcus_peter@gmx.de>
2021-04-01 16:23:31 +0200

    implemented here different major changes

--- Sonstige_Tools/Geocoder.py
+++ Sonstige_Tools/Geocoder.py
@@ -11,53 +11,62 @@
 from opencage.geocoder import OpenCageGeocode
 import geocoder
 import openpyxl
+import requests
 import time
+start_time = time.time()
+
 from pathlib import Path
 path = Path.home() / 'python32' / 'python_dir.txt'
 f = open(path, mode='r')
 for i in f: path = i
-path = Path.joinpath(Path(r'C:'+path),'GIS_Tools','Geocoder.txt')
+path = Path.joinpath(Path(path),'GIS_Tools','Geocoder.txt')
 f = path.read_text()
 f = f.split('\n')
-
-excelfile = "C:"+f[0]
 
 
 #--connecting to opencagegeocode--#
 key_osm = f[1]
 key_bing = f[2]
+key_here = f[3]
 geocoder_osm = OpenCageGeocode(key_osm)
 
 #--open excel--#
+excelfile = f[0]
 wbk = openpyxl.load_workbook(excelfile)
 wks = wbk.worksheets[0]
-number_r = len(wks.rows) ##number of rows
-number_c = 4 ##number of columns
 
-#--loop over adresses--#
-for r in range(1,number_r+1):##to prevent 0 as starting number
-    if r==1:continue ##dont need header
-    query = ""
-    for c in range(1,number_c+1):
-        if c==1:continue ##dont need the name
-        #query
-        try:query = query+" "+wks.cell(row=r, column=c).value
-        except:query = query+" "+str(wks.cell(row=r, column=c).value)
-
-    #--geocode--#
-##    result_osm = geocoder_osm.geocode(query)
-    result_bing = geocoder.bing(query, key=key_bing)
-
+#--geocoding--#
+for row in range(1,wks.max_row+1):##to prevent 0 as starting number
+    if row == 1:continue ##no header
+    result_bing = geocoder.bing(wks.cell(row,1).value, key=key_bing)
     try:
-##        wks.cell(row=r, column=5).value = result_osm[0]["geometry"]["lat"]
-##        wks.cell(row=r, column=6).value = result_osm[0]["geometry"]["lng"]
-        wks.cell(row=r, column=5).value = result_bing.lat
-        wks.cell(row=r, column=6).value = result_bing.lng
-        wks.cell(row=r, column=7).value = result_bing.postal
+        wks.cell(row, 2).value = result_bing.lat
+        wks.cell(row, 3).value = result_bing.lng
+        wks.cell(row, 4).value = int(result_bing.postal)
+        if result_bing.postal in wks.cell(row,1).value: wks.cell(row, 5).value = 0       
+        
+        else:
+            wks.cell(row, 5).value = 1
+            result_osm = geocoder_osm.geocode(wks.cell(row,1).value)
+            wks.cell(row, 6).value = result_osm[0]["geometry"]["lat"]
+            wks.cell(row, 7).value = result_osm[0]["geometry"]["lng"]
+                                   
     except: continue
-    time.sleep(1.1) ##pause 1.5 seconds
-    print(r)
+    time.sleep(0.1) ##pause 1.5 seconds
+    print(row)
+
+#here (not implemented yet)
+# URL = "https://geocode.search.hereapi.com/v1/geocode"
+# PARAMS = {'apikey':key_here,'q':wks.cell(row,1).value}
+# r = requests.get(url = URL, params = PARAMS)
+# data = r.json()
+# data['items'][0]['position']['lat']
+# data['items'][0]['position']['lng']
+
 
 #--end--#
 wbk.save(excelfile)
-print("fertig")
+wbk.close()
+
+seconds = int(time.time() - start_time)
+print("--finished after ",seconds,"seconds--")
