9008c97b2d814d3e57552b0b26ead81b1ea6c38a
mapeee <marcus_peter@gmx.de>
2021-11-18 14:56:45 +0100

    little changes

--- 10Miscellaneous/Geocoder.py
+++ 10Miscellaneous/Geocoder.py
@@ -15,14 +15,11 @@ import requests
 from pyproj import Transformer
 import time
 start_time = time.time()
-
 from pathlib import Path
-path = Path.home() / 'python32' / 'python_dir.txt'
-f = open(path, mode='r')
+f = open(Path.home() / 'python32' / 'python_dir.txt', mode='r')
 for i in f: path = i
 path = Path.joinpath(Path(path),'GIS_Tools','Geocoder.txt')
-f = path.read_text()
-f = f.split('\n')
+f = path.read_text().split('\n')
 
 #--connecting to opencagegeocode--#
 osm_key = f[1]
@@ -43,7 +40,7 @@ wks = wbk.worksheets[0]
 def bing(query):
     result_bing = geocoder.bing(query, key = bing_key)
     return result_bing.lng, result_bing.lat, 'bing', result_bing.postal, result_bing.raw["confidence"]
-    
+
 def geocage(query):
     result_osm = geocoder_osm.geocode(query)
     for i in result_osm:
@@ -71,24 +68,24 @@ def transform(XY):
     x_out, y_out = transformer.transform(XY[0], XY[1])
     return x_out, y_out
 
-def exl_writer(results, row):    
+def exl_writer(results, row):
     wks.cell(row, 4).value = transform(results)[0]
     wks.cell(row, 5).value = transform(results)[1]
     wks.cell(row, 6).value = results[2]
-    
-    
+
+
 #--geocoding--#
 for row in range(2,wks.max_row+1):##to prevent 0 and no header
 
-    query = wks.cell(row,Street).value+", "+str(wks.cell(row,ZIP).value)+" "+wks.cell(row,City).value      
+    query = wks.cell(row,Street).value+", "+str(wks.cell(row,ZIP).value)+" "+wks.cell(row,City).value
     #bing
     result = bing(query)
     #geocage
     if result[3] == None or result[3] not in query or result[4] != "High": result = geocage(query)
     #here
-    if result[3] == 'error': result = here(query)  
-    
-    exl_writer(result,row)       
+    if result[3] == 'error': result = here(query)
+
+    exl_writer(result,row)
     time.sleep(0.1) ##pause 1.5 seconds
     if row in list(range(0,wks.max_row+1,10)): print(row)
 
