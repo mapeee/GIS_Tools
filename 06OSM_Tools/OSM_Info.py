2f6f66e564135f5da48f3e78f75666098c9f1545
mapeee <marcus_peter@gmx.de>
2023-08-07 16:49:10 +0200

    - fix: Changed name of variable

--- 06OSM_Tools/OSM_Info.py
+++ 06OSM_Tools/OSM_Info.py
@@ -16,13 +16,13 @@ import overpy
 osmID = int(arcpy.GetParameterAsText(0))
 osm_type = arcpy.GetParameterAsText(1)
 
-def result_api(osmID, osm_type):
+def overpass_api(osmID, osm_type):
     query_api = osm_type+"(id:"+str(osmID)+"); (._;>;); out body;"
     result = overpy.Overpass().query(query_api)
     return result
 
 def result_print(osmID, osm_type):
-    result = result_api(osmID, osm_type)
+    result = overpass_api(osmID, osm_type)
     if osm_type == "way":
         for tag in result.get_way(osmID).tags:
             value = result.get_way(osmID).tags.get(tag, "n/a")
