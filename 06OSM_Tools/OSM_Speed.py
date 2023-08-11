56a221fa46908faad4de0dcdfec298892fbccfcc
mapeee <marcus_peter@gmx.de>
2023-08-11 14:28:05 +0200

    - new: travel directions

--- 06OSM_Tools/OSM_Speed.py
+++ 06OSM_Tools/OSM_Speed.py
@@ -30,44 +30,47 @@ def field_test(FC, tags):
         arcpy.AddMessage("> All tags existing")
 
 def osm_dict(id_field):
-    tags = [id_field,"vBike","tBike","vWalk","tWalk","Meter",
+    tags = [id_field,"vBike_FT","tBike_FT","vWalk_FT","tWalk_FT","vBike_TF","tBike_TF","vWalk_TF","tWalk_TF","Meter",
                 "access","highway","bike_osm","walk_osm","service","step_count"]
     tags = dict(zip(tags, [*range(0, len(tags))]))
     return tags
     
 def speed(data, tags):
-    vbike = 15
-    vwalk = 5
+    vbike = 17
+    vwalk = 4.8
     #--push the bike--#
     if data[tags["bike_osm"]] == "no":
         vbike = 4
-    if data[tags["highway"]] == "footway":
+    if data[tags["highway"]] in ["footway", "pedestrian"]:
         vbike = 4
+        vwalk = 5
     if data[tags["highway"]] == "steps":
         vbike = 2
         vwalk = 2
     if data[tags["highway"]] == "cycleway":
-        vbike = 20
-    data[tags["vBike"]] = vbike
-    data[tags["vWalk"]] = vwalk
+        vbike = 19
+    data[tags["vBike_FT"]], data[tags["vBike_TF"]] = vbike, vbike
+    data[tags["vWalk_FT"]], data[tags["vWalk_TF"]] = vwalk, vwalk
     
 def time(id_field, osm_type, data, tags):
-    vbike = data[tags["vBike"]]
-    vwalk = data[tags["vWalk"]]
+    vbike_FT, vbike_TF = data[tags["vBike_FT"]], data[tags["vBike_TF"]]
+    vwalk_FT, vwalk_TF = data[tags["vWalk_FT"]], data[tags["vWalk_TF"]]
     meter = data[tags["Meter"]]
     
-    data[tags["tBike"]] = (meter/(vbike/3.6))/60
-    data[tags["tWalk"]] = (meter/(vwalk/3.6))/60
+    data[tags["tBike_FT"]], data[tags["tBike_TF"]] = (meter/(vbike_FT/3.6))/60, (meter/(vbike_TF/3.6))/60
+    data[tags["tWalk_FT"]], data[tags["tWalk_TF"]] = (meter/(vwalk_FT/3.6))/60, (meter/(vwalk_TF/3.6))/60
     
     #--steps--#
     if data[tags["highway"]] == "steps":
-        data[tags["tBike"]] = data[tags["tBike"]]+0.25 #15s to sit on and off
+        data[tags["tBike_FT"]], data[tags["tBike_TF"]] = data[tags["tBike_FT"]]+0.25, data[tags["tBike_TF"]]+0.25 #15s to sit on and off
         try:
-            data[tags["tBike"]] = data[tags["tBike"]]+(int(data[tags["step_count"]])*(0.85/60))
-            data[tags["tWalk"]] = data[tags["tWalk"]]+(int(data[tags["step_count"]])*(0.65/60))
+            data[tags["tBike_FT"]] = data[tags["tBike_FT"]]+(int(data[tags["step_count"]])*(0.85/60))
+            data[tags["tBike_TF"]] = data[tags["tBike_TF"]]+(int(data[tags["step_count"]])*(0.85/60))
+            data[tags["tWalk_FT"]] = data[tags["tWalk_FT"]]+(int(data[tags["step_count"]])*(0.65/60))
+            data[tags["tWalk_TF"]] = data[tags["tWalk_TF"]]+(int(data[tags["step_count"]])*(0.65/60))
         except:
-            data[tags["tBike"]] = data[tags["tBike"]]+0.1
-            data[tags["tWalk"]] = data[tags["tWalk"]]+0.1
+            data[tags["tBike_FT"]], data[tags["tBike_TF"]] = data[tags["tBike_FT"]]+0.1, data[tags["tBike_TF"]]+0.1
+            data[tags["tWalk_FT"]], data[tags["tWalk_TF"]] = data[tags["tWalk_FT"]]+0.1, data[tags["tWalk_TF"]]+0.1
 
 #--editing--#        
 osm_tags = osm_dict(osm_id_field)    
