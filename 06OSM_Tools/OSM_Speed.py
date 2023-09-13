9d1dda201ce21aa26b5df3de4317595f81a4b34f
mapeee <marcus_peter@gmx.de>
2023-09-13 11:23:42 +0200

    - added: speed for car

--- 06OSM_Tools/OSM_Speed.py
+++ 06OSM_Tools/OSM_Speed.py
@@ -2,7 +2,7 @@
 # -*- coding: cp1252 -*-
 #-------------------------------------------------------------------------------
 # Name:        OSM Speed
-# Purpose:     Speed in (non)motorized transport networks
+# Purpose:     Speed in (non-)motorized transport networks
 # Author:      mape
 # Created:     07/08/2023
 # Copyright:   (c) mape 2023
@@ -35,10 +35,12 @@ def field_test(FC, tags):
 def linktime(id_field, data, tags):
     vbike_FT, vbike_TF = data[tags["vBike_FT"]], data[tags["vBike_TF"]]
     vwalk_FT, vwalk_TF = data[tags["vWalk_FT"]], data[tags["vWalk_TF"]]
-    meter = data[tags["Meter"]]
+    vcar = data[tags["vCar"]]
+    meter = data[tags["Shape_Length"]]
     
     data[tags["tBike_FT"]], data[tags["tBike_TF"]] = (meter/(vbike_FT/3.6))/60, (meter/(vbike_TF/3.6))/60
     data[tags["tWalk_FT"]], data[tags["tWalk_TF"]] = (meter/(vwalk_FT/3.6))/60, (meter/(vwalk_TF/3.6))/60
+    data[tags["tCar"]] = (meter/(vcar/3.6))/60
     
     #--steps--#
     if data[tags["highway"]] == "steps":
@@ -53,27 +55,39 @@ def linktime(id_field, data, tags):
             data[tags["tWalk_FT"]], data[tags["tWalk_TF"]] = data[tags["tWalk_FT"]]+0.1, data[tags["tWalk_TF"]]+0.1
 
 def nodetime(data, tags):
-    if data[tags["fclass"]] == "traffic_signals": data[tags["tBike"]], data[tags["tWalk"]] = 10, 0
+    if data[tags["fclass"]] == "traffic_signals": data[tags["tBike"]], data[tags["tWalk"]], data[tags["tCar"]] = 10, 0, 10
     elif data[tags["fclass"]] == "crossing":
-        if data[tags["crossing"]] == "traffic_signals": data[tags["tBike"]], data[tags["tWalk"]] = 4, 5
-        elif data[tags["crossing"]] == "marked": data[tags["tBike"]], data[tags["tWalk"]] = 3, 2
-        elif data[tags["crossing"]] == "zebra": data[tags["tBike"]], data[tags["tWalk"]] = 3, 2
-        else: data[tags["tBike"]], data[tags["tWalk"]] = 0, 0
-    else: data[tags["tBike"]], data[tags["tWalk"]] = 0, 0
+        if data[tags["crossing"]] == "traffic_signals": data[tags["tBike"]], data[tags["tWalk"]], data[tags["tCar"]] = 4, 5, 4
+        elif data[tags["crossing"]] == "marked": data[tags["tBike"]], data[tags["tWalk"]], data[tags["tCar"]] = 3, 2, 3
+        elif data[tags["crossing"]] == "zebra": data[tags["tBike"]], data[tags["tWalk"]], data[tags["tCar"]] = 3, 2, 3 
+        else: data[tags["tBike"]], data[tags["tWalk"]], data[tags["tCar"]] = 0, 0, 0 
+    else: data[tags["tBike"]], data[tags["tWalk"]], data[tags["tCar"]] = 0, 0, 0
 
 def osm_dict(id_field):
     if geo_type == "Polyline":
-        tags = [id_field,"vBike_FT","tBike_FT","vWalk_FT","tWalk_FT","vBike_TF","tBike_TF","vWalk_TF","tWalk_TF","Meter",
-                    "highway","bicycle","foot","step_count","cycleway", "sidewalk", "bike_l", "bike_r", "surface",
-                    "walk_l", "walk_r"]
+        tags = [id_field,"vBike_FT","tBike_FT","vWalk_FT","tWalk_FT","vBike_TF","tBike_TF","vWalk_TF","tWalk_TF","Shape_Length",
+                "vCar","tCar",
+                "highway","bicycle","foot","step_count","cycleway", "sidewalk", "bike_l", "bike_r", "surface", "maxspeed",
+                "walk_l", "walk_r", "town"]
     if geo_type == "Point":
-        tags = [id_field, "tBike", "tWalk", "fclass", "crossing"]
+        tags = [id_field, "tBike", "tWalk", "tCar", "fclass", "crossing"]
     tags = dict(zip(tags, [*range(0, len(tags))]))
     return tags
     
 def speed(data, tags):
     vbike = [17, 17]
     vwalk = [4.8, 4.8]
+    
+    #--maxspeed car--#
+    if data[tags["maxspeed"]] == 0 and data[tags["town"]] == 0:
+        try: vcar = {'motorway': 120, 'tertiary': 70, 'secondary': 80, 'residential': 25,
+                     'motorway_link': 30, 'primary': 100}[data[tags["highway"]]]
+        except: vcar = 15
+    elif data[tags["maxspeed"]] == 0 and data[tags["town"]] == 1:
+        try: vcar = {'motorway': 80, 'tertiary': 40, 'secondary': 45, 'residential': 25,
+                     'motorway_link': 30, 'primary': 50}[data[tags["highway"]]]
+        except: vcar = 15
+    else: vcar = data[tags["maxspeed"]]
     
     #--highway--#
     if data[tags["highway"]] in ["footway", "pedestrian"]: vbike, vwalk = [4,4], [5,5]
@@ -102,14 +116,17 @@ def speed(data, tags):
     if data[tags["walk_l"]] in ["no", "separate"]: vwalk[1] = min(vwalk[1], 3)
     vwalk[0], vwalk[1] = max(vwalk), max(vwalk)
     #--surface--#
-    if data[tags["surface"]] in ["asphalt", "concrete"]: vbike[0], vbike[1] = vbike[0]*1.05, vbike[1]*1.05
-    if data[tags["surface"]] in ["compacted"]: vbike[0], vbike[1] = vbike[0]*0.95, vbike[1]*0.95
-    if data[tags["surface"]] in ["ground", "unpaved", "pebblestone", "gravel"]: vbike[0], vbike[1] = vbike[0]*0.9, vbike[1]*0.9
-    if data[tags["surface"]] in ["dirt", "grass", "cobblestone", "earth"]: vbike[0], vbike[1] = vbike[0]*0.85, vbike[1]*0.85
-    if data[tags["surface"]] in ["mud", "sand"]: vbike[0], vbike[1] = vbike[0]*0.7, vbike[1]*0.7
+    if data[tags["surface"]] in ["asphalt", "concrete"]: vbike[0], vbike[1], vcar = vbike[0]*1.05, vbike[1]*1.05, vcar*1.05
+    if data[tags["surface"]] in ["compacted"]: vbike[0], vbike[1], vcar = vbike[0]*0.95, vbike[1]*0.95, vcar*0.95
+    if data[tags["surface"]] in ["ground", "unpaved", "pebblestone", "gravel"]: vbike[0], vbike[1], vcar = vbike[0]*0.9, vbike[1]*0.9, vcar*0.9
+    if data[tags["surface"]] in ["dirt", "grass", "cobblestone", "earth"]: vbike[0], vbike[1], vcar = vbike[0]*0.85, vbike[1]*0.85, vcar*0.85
+    if data[tags["surface"]] in ["mud", "sand"]: vbike[0], vbike[1], vcar = vbike[0]*0.7, vbike[1]*0.7, vcar*0.7
+    #--car--#
+    if vcar > 70: vcar = max(vcar*0.85,70)
     
     data[tags["vBike_FT"]], data[tags["vBike_TF"]] = vbike[0], vbike[1]
     data[tags["vWalk_FT"]], data[tags["vWalk_TF"]] = vwalk[0], vwalk[1]
+    data[tags["vCar"]] = vcar
 
 #--editing--#        
 osm_tags = osm_dict(osm_id_field)    
