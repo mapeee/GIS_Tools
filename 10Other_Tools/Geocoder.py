4bf3fd9577e94ee4f32138a9f44e9e88d61c841f
mapeee <marcus_peter@gmx.de>
2024-09-04 17:01:17 +0200

    - some minor changes

--- 10Other_Tools/Geocoder.py
+++ 10Other_Tools/Geocoder.py
@@ -28,9 +28,12 @@ here_key = f[3]
 geocoder_osm = OpenCageGeocode(osm_key)
 
 #--values--#
-Street = 9
-ZIP = 12
-City = 13
+Street = 3
+ZIP = 4
+City = 5
+
+#--writer--#
+x,y,coder = 12, 13, 14
 
 #--open excel--#
 excelfile = f[0]
@@ -38,28 +41,34 @@ wbk = openpyxl.load_workbook(excelfile)
 wks = wbk.worksheets[0]
 
 def bing(query):
-    result_bing = geocoder.bing(query, key = bing_key)
-    return result_bing.lng, result_bing.lat, 'bing', result_bing.postal, result_bing.raw["confidence"]
+    try: 
+        result_bing = geocoder.bing(query, key = bing_key)
+        if result_bing.postal == None or result_bing.postal not in query or result_bing.raw["confidence"] != "High":
+            return [None]
+        else: return result_bing.lng, result_bing.lat, 'bing'
+    except: return [None]
 
 def geocage(query):
     result_osm = geocoder_osm.geocode(query)
     for i in result_osm:
         try:
-            no = i["components"]["house_number"]
             lat = i["geometry"]["lat"]
             lng = i["geometry"]["lng"]
             break
-        except: lat, lng, no = "error", "error", "error"
-    return lng, lat, 'opencage', no
+        except: return [None]
+    return lng, lat, 'opencage'
 
 def here(query):
     URL = "https://geocode.search.hereapi.com/v1/geocode"
     PARAMS = {'apikey':here_key,'q':query}
-    r = requests.get(url = URL, params = PARAMS)
-    data = r.json()
-    lat = data['items'][0]['position']['lat']
-    lng = data['items'][0]['position']['lng']
-    return lng, lat, 'here'
+    try:
+        r = requests.get(url = URL, params = PARAMS)
+        data = r.json()
+        lat = data['items'][0]['position']['lat']
+        lng = data['items'][0]['position']['lng']
+        return lng, lat, 'here'
+    except:
+        return 0, 0, 'error'
 
 def transform(XY):
     epsg_in = "epsg:4326"
@@ -69,32 +78,31 @@ def transform(XY):
     return x_out, y_out
 
 def exl_writer(results, row):
-    wks.cell(row, 4).value = transform(results)[0]
-    wks.cell(row, 5).value = transform(results)[1]
-    wks.cell(row, 6).value = results[2]
+    wks.cell(row, x).value = transform(results)[0]
+    wks.cell(row, y).value = transform(results)[1]
+    wks.cell(row, coder).value = results[2]
 
 
 #--geocoding--#
 for row in range(2,wks.max_row+1):##to prevent 0 and no header
-
     if wks.cell(row,Street).value == None: break
 
     query = wks.cell(row,Street).value+", "+str(wks.cell(row,ZIP).value)+" "+wks.cell(row,City).value
     #bing
     result = bing(query)
     #geocage
-    if result[3] == None or result[3] not in query or result[4] != "High": result = geocage(query)
+    if result[0] == None: result = geocage(query)
     #here
-    if result[3] == 'error': result = here(query)
+    if result[0] == None: result = here(query)
 
     exl_writer(result,row)
     time.sleep(0.1) ##pause 1.5 seconds
     if row in list(range(0,wks.max_row+1,10)): print(row)
 
 #--header--#
-wks.cell(1, 16).value = 'X'
-wks.cell(1, 17).value = 'Y'
-wks.cell(1, 18).value = 'geocoder'
+wks.cell(1, x).value = 'X'
+wks.cell(1, y).value = 'Y'
+wks.cell(1, coder).value = 'geocoder'
 
 #--end--#
 wbk.save(excelfile)
