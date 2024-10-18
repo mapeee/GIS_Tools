38a1fba1fc70e6ae1783621ef0fa4beda8cfba8b
mapeee <marcus_peter@gmx.de>
2024-10-18 16:21:01 +0200

    - many changes

--- 10Other_Tools/OSM_Geocoder.py
+++ 10Other_Tools/OSM_Geocoder.py
@@ -11,23 +11,31 @@ start_time = time.time()
 
 f = open(Path.home() / 'python32' / 'python_dir.txt', mode='r')
 path = list(f)[0]
-path = Path.joinpath(Path(path),'GIS_Tools','OSM_Geocoder.txt')
+path = Path.joinpath(Path(path), 'GIS_Tools', 'OSM_Geocoder.txt')
 f = path.read_text().split('\n')
 
-#--connections--#
+# --connections--#
 geodata = gpd.read_file(f[0])
 addr = pd.read_excel(f[1])
 
-#--values--#
+# --values--#
 STREET = 'NL.ADR-STR-PF'
 HOUSE = 'NL.ADR-HNR'
 ZIP = 'NL.ADR-PLZ'
 CITY = 'NL.ADR-ORT'
 
-#--prepare--#
-addr.sort_values(by=[CITY,ZIP,STREET], inplace=True)
+# --prepare--#
+addr.sort_values(by=[CITY, ZIP, STREET], inplace=True)
 
-def geocoder(_addr_all,_geodata):  
+
+def geodata_prep(_geodata):
+    _geodata["HOUSENO"] = _geodata["HOUSENO"].str.replace(' ', '')
+    _geodata["HOUSENO"] = _geodata["HOUSENO"].str.replace(';', '/')
+    _geodata["HOUSENO"] = _geodata["HOUSENO"].str.lower()
+    return _geodata
+
+
+def geocoder(_addr_all, _geodata):
     """function to find street in city"""
     _city, _zip, _street0 = "_city", "00000", "_street"
     for _i in _addr_all.iterrows():
@@ -53,51 +61,56 @@ def geocoder(_addr_all,_geodata):
             if len(_data_street) == 0:
                 yield [0, 0, "error", "street missing"]
                 continue
-        values = geocode_house(_i,_data_street)
+        values = geocode_house(_i, _data_street)
         if values[2] == "error":
             _data_street = _data_city[_data_city["STREET"] == _street]
-            values = geocode_house(_i,_data_street)
+            values = geocode_house(_i, _data_street)
         yield values
+
 
 def geocode_house(_addr, _geodata_street):
     """function to find the house number"""
     _house = str(_addr[1][HOUSE])
+    _house = _house.lower().replace(" ", "")
     _note = ""
-    
+    if _house == "nan":
+        _house = ""
     if len(_house) > 9:
         return [0, 0, "error", "house format error"]
-    
     if _house not in _geodata_street["HOUSENO"].unique():
         if any(x in _house for x in ["-", " - ", "+", "/"]):
-            for delimiter in ["-", " - ", "/", "+"]:#split '- /'
+            for delimiter in ["-", " - ", "/", "+"]:  # split '- /'
                 _house = " ".join(_house.split(delimiter))
             _house = _house.split()[0]
-        _house = "".join(c for c in str(_house) if  c.isdecimal()) #remove letters e.g. '7c -> 7'
-        if _house not in _geodata_street["HOUSENO"].unique(): #different numbers (+/-10)
-            _house = str(max(int(_house)-10,1))
-            for n in range(1,19):
-                _house = str(int(_house) + 1)
-                if _house in _geodata_street["HOUSENO"].unique():
+        _house = "".join(c for c in str(_house) if c.isdecimal())  # remove letters e.g. '7c -> 7'
+        if _house not in _geodata_street["HOUSENO"].unique():  # different numbers (+/-10)
+            if _house == "":
+                return [0, 0, "error", "house missing"]
+            for n in range(1, 11):
+                if str(int(_house) + n) in _geodata_street["HOUSENO"].unique():
+                    _house = str(int(_house) + n)
                     break
-                if n == 19:
+                if str(max(int(_house) - n, 1)) in _geodata_street["HOUSENO"].unique():
+                    _house = str(max(int(_house) - n, 1))
+                    break
+                if n == 10:
                     return [0, 0, "error", "house missing"]
         _note = _house
-    
     _data_house = _geodata_street[_geodata_street["HOUSENO"] == str(_house)]
-    
     if str(_addr[1][ZIP]) not in _data_house["POSTCODE"].unique():
         if _note != "":
             _note = "zip: "+_data_house["POSTCODE"].iloc[0]+", house: "+_note
         else:
             _note = "zip: "+_data_house["POSTCODE"].iloc[0]
-    
-    return [_data_house.get_coordinates().iloc[0,0], _data_house.get_coordinates().iloc[0,1], "ok", str(_note)]
+    return [_data_house.get_coordinates().iloc[0, 0], _data_house.get_coordinates().iloc[0, 1], "ok", str(_note)]
 
-a = list(geocoder(addr,geodata))
-addr[["x","y","geocoder","note"]] = a
+
+geodata = geodata_prep(geodata)
+a = list(geocoder(addr, geodata))
+addr[["x", "y", "geocoder", "note"]] = a
 
 addr.to_excel(os.path.dirname(f[1])+"\\test2.xlsx")
 
-#--end--#
+# --end--#
 seconds = int(time.time() - start_time)
-print("--finished after ",seconds,"seconds--")
+print("--finished after ", seconds, "seconds--")
